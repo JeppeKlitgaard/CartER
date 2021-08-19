84e850f42af8cfec4ecaa855a3541174cbcfa6e4
Jeppe Klitgaard <jk782@cam.ac.uk>
2021-08-19 17:16:53 +0100

    Fix warnings and go to c++17

--- controller/include/CustomArduino.h
+++ controller/include/CustomArduino.h
@@ -1,4 +1,12 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wregister"
+
 #include <Arduino.h>
 
 #undef max
 #undef min
+
+#undef SERIAL_BUFFER_SIZE
+#define SERIAL_BUFFER_SIZE 512
+
+#pragma GCC diagnostic pop
